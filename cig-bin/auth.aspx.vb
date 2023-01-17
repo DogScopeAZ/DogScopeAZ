769ffde387c3e0f6355102516735a77fade9ae68
DogScopeAZ <102510778+DogScopeAZ@users.noreply.github.com>
2023-01-17 08:19:41 -0700

    Delete cig-bin directory

--- cig-bin/auth.aspx.vb
+++
@@ -1,9 +0,0 @@
-Public Class auth
-    Inherits System.Web.UI.Page
-
-    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-        Me.Controls.Clear()
-        Response.Write("")
-    End Sub
-
-End Class
